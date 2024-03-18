b3b25887f228477bc8a48da497711cb470d41bc2
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-18 02:01:06 -0500

    update shorten url page

--- src/app/shorten-url/page.tsx
+++ src/app/shorten-url/page.tsx
@@ -1,9 +1,26 @@
+'use client'
 import React from 'react'
+import TopNavBar from '../components/TopNavBar';
+import UrlShortenerDisplay from '../components/UrlShortnerDisplay';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase/firebase.config';
 
 const ShortenUrl = () => {
-  return (
-    <div>ShortenUrl</div>
-  )
-}
+const [user] = useAuthState(auth);
 
+  const handleSignOut = () => {
+    console.log('Signing out...');
+    auth.signOut();
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50 p-4">
+      <TopNavBar isLoggedIn={!!user} handleSignOut={handleSignOut} />
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-8">Shorten Your URL</h1>
+        <UrlShortenerDisplay />
+      </div>
+    </div>
+  );
+};
 export default ShortenUrl;
